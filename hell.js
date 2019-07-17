cbc0bd139ce443c72d22a911ed150c79e59d656b
Jack Guinane <jackguinane@gmail.com>
2019-07-17 15:21:06 -0400

    Improve animations

--- hell.js
+++ hell.js
@@ -2,66 +2,74 @@
 /*
     these functions generate the animation on start
 */
-function isWs(c){
-    var is_whitespace = /\s/;
-    return is_whitespace.test(c);
+function sleep(ms) 
+{
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function jumbleArray(bin_arr, final_arr, final_threshold){
-    var ignore = false;
-    for(var i = 0; i < bin_arr.length; i++)
-    {               
-        if(bin_arr[i] == '<'){
-            ignore = true;
-        }
-        else if(bin_arr[i] == '>'){
-            ignore = false;
-        }
-        else if(!ignore && !isWs(bin_arr[i])){
-            var final_rand = Math.random();
-            var bin_rand = Math.round(Math.random());
-            if(final_rand > final_threshold)
+async function animateName()
+{
+    var name = document.getElementById("name");
+    var finalName = name.innerHTML;
+    name.innerHTML = "_";
+    await sleep(100);
+
+    for(var k = 0; k < finalName.length; k++)
+    {
+        await sleep(10 + (Math.pow(k, 2)/2));
+        name.innerHTML = finalName.slice(0, k) + "_";
+    }
+
+    await sleep(10+(Math.pow(finalName.length,2)/2));
+    name.innerHTML = finalName;
+}
+
+async function animateBio()
+{
+    var char = "1234567890-=!@#$%&*()[]{}\|qwertyuiopasdfghjklzxcvbnm>?QWERTYUIOPLKJHGFDSAZXCVBNM".split("");
+    var bio = document.getElementById("bio");
+    var finalTxt = bio.innerHTML.split("");
+    var txt = [" "]
+    bio.innerHTML = txt;
+
+    var half = Math.floor(finalTxt.length / 3);
+    await sleep(500);
+
+    for(var i = 0; i < (finalTxt.length + half); i++)
+    {
+        await sleep(10 + (Math.pow(i / 50, 4))/4);
+
+        if(i < finalTxt.length)
+        {
+            if("., '".includes(finalTxt[i]))
             {
-                if(bin_rand == 0){
-                    bin_arr[i] = '0';
-                }
-                else{
-                    bin_arr[i] = '1';
-                }
+                txt[i] = finalTxt[i];
             }
             else
             {
-                bin_arr[i] = final_arr[i];
+                txt[i] = char[Math.floor(Math.random()*char.length)];
             }
         }
-    }
 
-    return bin_arr;
-}
-
-function binaryFlipAnimation(id)
-{
-    var elem = document.getElementById(id);
-    var final_str = elem.innerHTML;
-    var final_arr = final_str.split("");
-    var binary_str = final_str;
-    var binary_arr = binary_str.split("");
-    
-    var num_times = 0;
-    var final_threshold = 0;
-    var animate = setInterval(animateBinary, 150);
-    function animateBinary(){
-        if(num_times == 20){
-            elem.innerHTML = final_str;
+        if(i % 4 == 0)
+        {
+            for(var j = Math.max(0, (i - half)); j < Math.min(finalTxt.length, i); j++)
+            {
+                if(!"., '".includes(finalTxt[j]))
+                {
+                    txt[j] = char[Math.floor(Math.random()*char.length)];
+                }
+            }
         }
-        else{
-            num_times = num_times + 1;
-            final_threshold = final_threshold + 0.065;
-            elem.innerHTML = jumbleArray(
-                                binary_arr,
-                                final_arr,
-                                final_threshold).join("");                               
+
+        if(i >= half)
+        {
+            txt[i - half] = finalTxt[Math.floor(i - half)];
+            bio.innerHTML = "<span class='done'>" + txt.join("").slice(0, i - half) + "</span>" + txt.join("").slice(i - half);
+        }
+        else
+        {
+            bio.innerHTML = txt.join("");
         }
     }
-   
 }
